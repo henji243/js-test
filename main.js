5550c822ae0983416a8b4775c1527b14efaa8e0a
henji <105550500+henji243@users.noreply.github.com>
2022-09-20 21:43:45 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,15 +2,15 @@
 const sleep = (waitTime) =>
   new Promise((resolve) => setTimeout(resolve, waitTime));
 
-var words = "HelloWorld!".repeat(9);
+var words = "HelloWorld!".repeat(15);
 const main = async function () {
   while (true) {
     for (const elem of words) {
-      document.write(elem.fontsize(6)); // どうやら非推奨らしい
-      await sleep(25);
+      document.write(elem.fontsize(7)); // どうやら非推奨らしい
+      await sleep(10);
     }
 
-    document.write("</br>".fontsize(6));
+    document.write("</br>".fontsize(7));
   }
 };
 main();
